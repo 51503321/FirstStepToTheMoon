23b297342ec6cbb4d681536f89f4e29cf0253604
Thien <33951318+51503321@users.noreply.github.com>
2025-02-21 14:38:53 +0700

    asdasd

--- frontend/Rocker/eslint.config.mjs
+++ frontend/Rocker/eslint.config.mjs
@@ -1,20 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      // Note: you must disable the base rule as it can report incorrect errors
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            // Note: you must disable the base rule as it can report incorrect errors
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'error',
+        },
     },
-  },
 ];
--- frontend/Rocker/src/App.tsx
+++ frontend/Rocker/src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 // default constants
 
 const App = () => {
-    const age = 'asdaidnkad';
-    const baaaa = 'abc';
-    const i = 1921313;
-    const adsada = 1921313;
-    const adsada111 = 1921313;
-    const adsada111222222 = 1921313;
-    const adsada11122222 = 1921313222;
+    const age = 'John';
+    const adsada = 1921313222;
     console.log(age);
     return <></>;
 };
