748c833b20b1ceb6237be5d6383ec2b7ed67b334
Thien <33951318+51503321@users.noreply.github.com>
2025-02-21 14:24:12 +0700

    Keep calm and commit

--- frontend/Rocker/src/App.tsx
+++ frontend/Rocker/src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     const i = 1921313;
     const adsada = 1921313;
     const adsada111 = 1921313;
-    const adsada111222 = 1921313;
+    const adsada111222222 = 1921313;
     const adsada11122222 = 1921313222;
     console.log(age);
     return <></>;
