e06d3b91d592199236d10dae97c9a6e75cb33d5b
Thien <33951318+51503321@users.noreply.github.com>
2025-02-21 16:32:05 +0700

    a

--- frontend/Rocker/src/App.tsx
+++ frontend/Rocker/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 // default constants
 const App = () => {
-    const age = 'John';
-    const s = 'asd';
-    console.log(age);
-    var ss = '';
-    return <></>;
+    return <>2</>;
 };
 
 export default App;
