3c6558632b2193c3190a725aef276d032ec9ef99
Thien <33951318+51503321@users.noreply.github.com>
2025-02-21 15:53:36 +0700

    asdas

--- frontend/Rocker/eslint.config.mjs
+++ frontend/Rocker/eslint.config.mjs
@@ -10,4 +10,11 @@ export default [
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            // Note: you must disable the base rule as it can report incorrect errors
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+        },
+    },
 ];
--- frontend/Rocker/src/App.tsx
+++ frontend/Rocker/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 // default constants
-
 const App = () => {
     const age = 'John';
+    const s = 'asd';
     console.log(age);
+    var ss = '';
     return <></>;
 };
 
